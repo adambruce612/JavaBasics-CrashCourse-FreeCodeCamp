a39551be1e917c1f23fcd87c56a13795f2b5e920
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-10-30 21:09:38 -0500

    Add files via upload

--- ControlFlow/src/ControlFlow.java
+++ ControlFlow/src/ControlFlow.java
@@ -2,7 +2,7 @@ public class ControlFlow
 {
     public static void main(String[] args)
     {
-        int age = 12;
+        int age = 17;
 
         if (age >= 18)
         {
@@ -10,7 +10,51 @@ public class ControlFlow
             System.out.println("Please put an X in the ballot.");
         } else
         {
-            System.out.println("You can't vote; \nPlease come in " + (18 - age) + " years.");
+            if (18 - age == 1)
+            {
+                System.out.println("You can't vote; \nPlease come back in " + (18 - age) + " year.");
+            }
+            System.out.println("You can't vote; \nPlease come back in " + (18 - age) + " years.");
+        }
+
+        int year = 2022;
+
+        if (year != 2019)
+        {
+            System.out.println("The year is = " + year);
+        }
+        else if (year == 2020)
+        {
+            System.out.println("The year is = " + year);
+        }
+        else if (year == 2022)
+        {
+            System.out.println("The year is = " + year);
+        }
+        else
+        {
+            System.out.println("All conditions failed.");
+        }
+
+        boolean isActive = true;
+        int myNumber = 20;
+
+        if (!isActive)
+        {
+            System.out.println("The user is active.");
+        }
+        else
+        {
+            System.out.println("The user is not active.");
+        }
+
+        if (!(myNumber >= 10))
+        {
+            System.out.println("The user is active.");
+        }
+        else
+        {
+            System.out.println("The user is not active.");
         }
     }
 }
